10475da84ebfce42a839a12c6f9d19d81d25e8a7
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-11-18 17:39:04 +0700

    update: Post Process Highlight with Enemy Detect by Mouse Cursor

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -5,10 +5,78 @@
 
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true; // If true, this actor will replicate to remote machines
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+	
+	LastActor = CurrentActor;
+	CurrentActor = CursorHit.GetActor();
+	
+	/**
+	 * Line trace from cursor. There are several scenarios:
+	 *  A. LastActor is null && ThisActor is null
+	 *		- Do nothing
+	 *	B. LastActor is null && ThisActor is valid
+	 *		- Highlight ThisActor
+	 *	C. LastActor is valid && ThisActor is null
+	 *		- UnHighlight LastActor
+	 *	D. Both actors are valid, but LastActor != ThisActor
+	 *		- UnHighlight LastActor, and Highlight ThisActor
+	 *	E. Both actors are valid, and are the same actor
+	 *		- Do nothing
+	 */
+
+	if (LastActor == nullptr)
+	{
+		if (CurrentActor != nullptr)
+		{
+			// Case B
+			CurrentActor->HighlightActor();
+		}
+		else
+		{
+			// Case A - both are null, do nothing
+		}
+	}
+	else // LastActor is valid
+	{
+		// Case C
+		if (CurrentActor == nullptr)
+		{
+			LastActor->UnHighlightActor();
+		}
+		else // both actors are valid
+		{
+			if (LastActor != CurrentActor)
+			{
+				// Case D
+				LastActor->UnHighlightActor();
+				CurrentActor->HighlightActor();
+			}
+			else
+			{
+				// Case E - Do nothing
+			}
+		}
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("Current: %s"), *CursorHit.GetActor()->GetName());
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -22,6 +90,14 @@ void AAuraPlayerController::BeginPlay()
 	{
 		Subsystem->AddMappingContext(IMC_AuraCharacter, 0);
 	}
+	
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
 }
 
 void AAuraPlayerController::SetupInputComponent()
--- Source/Aura/Public/Character/Enemy.h
+++ Source/Aura/Public/Character/Enemy.h
@@ -4,14 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "Character/CharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "Enemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AEnemy : public ACharacterBase
+class AURA_API AEnemy : public ACharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+public:
+	AEnemy();
+	
+	//~ Enemy Interface
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+	//~ End Enemy Interface
+	
+protected:
+	virtual void BeginPlay() override;
 };
